d91df7029e207c823c116b54adfb95816dc37227
janshv <janshv.dmitry@mail.ru>
2026-01-14 20:21:40 +0300

    task_5_4_step_14 done

---
+++ playwright_registration.py
@@ -0,0 +1,28 @@
+from playwright.sync_api import sync_playwright, expect
+
+with sync_playwright() as playwright:
+
+    browser = playwright.chromium.launch(headless=True)
+    page = browser.new_page()
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+    registration_email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    expect(registration_email_input).to_be_visible()
+
+    registration_username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    expect(registration_username_input).to_be_visible()
+
+    registration_password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    expect(registration_password_input).to_be_visible()
+
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    expect(registration_button).to_be_visible()
+
+    registration_email_input.fill('user.name@gmail.com')
+    registration_username_input.fill('username')
+    registration_password_input.fill('password')
+    registration_button.click()
+
+    dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
+    expect(dashboard_title).to_be_visible()
+    expect(dashboard_title).to_have_text('Dashboard')
