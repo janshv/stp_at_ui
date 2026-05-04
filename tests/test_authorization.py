7feb54ad5a039ef36ea840b2f5cd9d101e76fb65
janshv <janshv.dmitry@mail.ru>
2026-05-04 17:07:11 +0300

    step_6_7_14 done

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -4,17 +4,21 @@ import pytest
 
 @pytest.mark.regression
 @pytest.mark.authorization
-def test_wrong_email_or_password_authorization(chromium_page: Page):
+@pytest.mark.parametrize("email, password", [
+    ('user.name@gmail.com', 'password'),
+    ('user.name@gmail.com', '  '),
+    ('  ', 'password')])
+def test_wrong_email_or_password_authorization(chromium_page: Page, email: str, password: str):
     # Переходим на страницу авторизации
     chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
     # Находим поле "Email" и заполняем его
     email_input = chromium_page.get_by_test_id('login-form-email-input').locator('input')
-    email_input.fill("user.name@gmail.com")
+    email_input.fill(email)
 
     # Находим поле "Password" и заполняем его
     password_input = chromium_page.get_by_test_id('login-form-password-input').locator('input')
-    password_input.fill("password")
+    password_input.fill(password)
 
     # Находим кнопку "Login" и кликаем на нее
     login_button = chromium_page.get_by_test_id('login-page-login-button')
