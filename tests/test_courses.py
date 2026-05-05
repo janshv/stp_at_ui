2876397cc13f72cbb549c1ad1c90263c3d585cd3
janshv <janshv.dmitry@mail.ru>
2026-05-05 18:53:02 +0300

    step_7_2_14 done

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,6 +1,9 @@
 from playwright.sync_api import sync_playwright, expect, Page
 import pytest
 
+from pages.courses_list_page import CoursesListPage
+from pages.create_course_page import CreateCoursePage
+
 
 @pytest.mark.courses
 @pytest.mark.regression
@@ -23,3 +26,25 @@ def test_empty_courses_list(chromium_page_with_state: Page):
     expect(desc_title).to_have_text('Results from the load test pipeline will be displayed here')
 
     chromium_page_with_state.wait_for_timeout(2500)
+
+
+@pytest.mark.courses
+@pytest.mark.regression
+def test_create_course(create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
+    create_course_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
+    create_course_page.check_visible_create_course_title()
+    create_course_page.check_disabled_create_course_button()
+    create_course_page.check_visible_image_preview_empty_view()
+    create_course_page.check_visible_image_upload_view()
+    create_course_page.check_visible_create_course_form("", "", "", "0", "0")
+    create_course_page.check_visible_exercises_title()
+    create_course_page.check_visible_create_exercise_button()
+    create_course_page.check_visible_exercises_empty_view()
+    create_course_page.upload_preview_image("testdata/files/image.png")
+    create_course_page.check_visible_image_upload_view(is_image_uploaded=True)
+    create_course_page.fill_create_course_form(title='Playwright', estimated_time='2 weeks', description='Playwright',
+                                               max_score='100', min_score='10')
+    create_course_page.click_create_course_button()
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_create_course_button()
+    courses_list_page.check_visible_course_card(index=0, title='Playwright', max_score='100', min_score='10', estimated_time='2 weeks')
